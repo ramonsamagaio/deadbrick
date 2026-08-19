7e9cc66f177057be8234a8c88fd2427c5c3d1a17
ramonsamagaio <70339182+ramonsamagaio@users.noreply.github.com>
2026-08-19 10:43:09 -0300

    Fix FPS movement and add reference player visuals

--- Source/Deadbrick/Player/DeadbrickCharacter.h
+++ Source/Deadbrick/Player/DeadbrickCharacter.h
@@ -4,6 +4,7 @@
 #include "GameFramework/Character.h"
 #include "DeadbrickCharacter.generated.h"
 
+class UAnimSequence;
 class UCameraComponent;
 class UFirearmComponent;
 
@@ -14,16 +15,31 @@ class DEADBRICK_API ADeadbrickCharacter : public ACharacter
 
 public:
     ADeadbrickCharacter();
+    virtual void Tick(float DeltaSeconds) override;
     virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
 
     UPROPERTY(VisibleAnywhere, BlueprintReadOnly) TObjectPtr<UCameraComponent> FirstPersonCamera;
     UPROPERTY(VisibleAnywhere, BlueprintReadOnly) TObjectPtr<UFirearmComponent> Firearm;
 
+protected:
+    virtual void BeginPlay() override;
+
 private:
+    UPROPERTY(Transient) TObjectPtr<UAnimSequence> IdleAnimation;
+    UPROPERTY(Transient) TObjectPtr<UAnimSequence> WalkAnimation;
+    UPROPERTY(Transient) TObjectPtr<UAnimSequence> CurrentAnimation;
+
+    FVector SafeSpawnLocation = FVector::ZeroVector;
+    FRotator SafeSpawnRotation = FRotator::ZeroRotator;
+    bool bHasSafeSpawn = false;
+
     void MoveForward(float Value);
     void MoveRight(float Value);
     void LookYaw(float Value);
     void LookPitch(float Value);
     void Fire();
     void Reload();
+    void TryApplyReferenceVisuals();
+    void UpdateReferenceAnimation();
+    void RecoverFromFall();
 };
