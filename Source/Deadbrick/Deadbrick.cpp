ccc753f3c273ece67ae1b8220f3b917947dea187
ramonsamagaio <70339182+ramonsamagaio@users.noreply.github.com>
2026-08-19 11:40:32 -0300

    Mark destructive reference parity build D

--- Source/Deadbrick/Deadbrick.cpp
+++ Source/Deadbrick/Deadbrick.cpp
@@ -7,7 +7,7 @@ public:
     virtual void StartupModule() override
     {
         FDefaultGameModuleImpl::StartupModule();
-        UE_LOG(LogTemp, Display, TEXT("DEADBRICK MODULE BUILD 2026-08-19-C REFERENCE-INTEGRATION LOADED"));
+        UE_LOG(LogTemp, Display, TEXT("DEADBRICK MODULE BUILD 2026-08-19-D DESTRUCTIVE-REFERENCE-PARITY LOADED"));
     }
 };
 
