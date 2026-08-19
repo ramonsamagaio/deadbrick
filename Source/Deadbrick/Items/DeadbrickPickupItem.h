b100154fc77db1ce7eea5470876484cc614ae0c2
ramonsamagaio <70339182+ramonsamagaio@users.noreply.github.com>
2026-08-19 11:29:53 -0300

    Add physical pickup actor for modern salvage drops

---
+++ Source/Deadbrick/Items/DeadbrickPickupItem.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Items/DeadbrickItemTypes.h"
+#include "World/DeadbrickVoxelTypes.h"
+#include "DeadbrickPickupItem.generated.h"
+
+class UStaticMeshComponent;
+
+UCLASS()
+class DEADBRICK_API ADeadbrickPickupItem : public AActor
+{
+    GENERATED_BODY()
+
+public:
+    ADeadbrickPickupItem();
+
+    UPROPERTY(BlueprintReadOnly, Category="Item")
+    EDeadbrickItemType ItemType = EDeadbrickItemType::ConcreteRubble;
+
+    UPROPERTY(BlueprintReadOnly, Category="Item")
+    int32 Quantity = 1;
+
+    UFUNCTION(BlueprintCallable, Category="Item")
+    void InitializeFromVoxelMaterial(EDeadbrickVoxelMaterial Material, int32 InQuantity);
+
+    UFUNCTION(BlueprintCallable, Category="Item")
+    int32 Collect(EDeadbrickItemType& OutType);
+
+private:
+    UPROPERTY(VisibleAnywhere)
+    TObjectPtr<UStaticMeshComponent> MeshComponent;
+};
