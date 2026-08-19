93828d683937ed906eae1c4bf2b70c28524908d3
ramonsamagaio <70339182+ramonsamagaio@users.noreply.github.com>
2026-08-19 11:32:56 -0300

    Expose pools of reference meshes for varied reused assets

--- Source/Deadbrick/Reference/ReferenceAssetResolver.h
+++ Source/Deadbrick/Reference/ReferenceAssetResolver.h
@@ -11,6 +11,8 @@ namespace DeadbrickReferenceAssets
 {
     DEADBRICK_API bool HasCookedReferenceAssets();
     DEADBRICK_API USkeletalMesh* FindSkeletalMesh(const TArray<FString>& PreferredKeywords, FString* OutObjectPath = nullptr);
+    DEADBRICK_API TArray<USkeletalMesh*> FindSkeletalMeshes(const TArray<FString>& PreferredKeywords, int32 MaxResults = 8);
     DEADBRICK_API UAnimSequence* FindAnimationForSkeleton(USkeleton* Skeleton, const TArray<FString>& PreferredKeywords, FString* OutObjectPath = nullptr);
     DEADBRICK_API UStaticMesh* FindStaticMesh(const TArray<FString>& PreferredKeywords, FString* OutObjectPath = nullptr);
+    DEADBRICK_API TArray<UStaticMesh*> FindStaticMeshes(const TArray<FString>& PreferredKeywords, int32 MaxResults = 12);
 }
