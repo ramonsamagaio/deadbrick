29b65515fd1aec99defd925e88b0b4825c566927
ramonsamagaio <70339182+ramonsamagaio@users.noreply.github.com>
2026-08-20 05:15:27 -0300

    Enable resource inventory HUD

--- Source/Deadbrick/DeadbrickGameModeBase.cpp
+++ Source/Deadbrick/DeadbrickGameModeBase.cpp
@@ -1,8 +1,10 @@
 #include "DeadbrickGameModeBase.h"
 #include "Player/DeadbrickCharacter.h"
+#include "UI/DeadbrickHUD.h"
 
 ADeadbrickGameModeBase::ADeadbrickGameModeBase()
 {
     DefaultPawnClass = ADeadbrickCharacter::StaticClass();
+    HUDClass = ADeadbrickHUD::StaticClass();
     bStartPlayersAsSpectators = false;
 }
