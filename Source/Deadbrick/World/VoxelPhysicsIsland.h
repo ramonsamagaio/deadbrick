5eec5a9a00f291d54a476c1ab8743e18096b9fe0
ramonsamagaio <70339182+ramonsamagaio@users.noreply.github.com>
2026-08-20 04:15:08 -0300

    Make collapsed rubble damageable and pushable

--- Source/Deadbrick/World/VoxelPhysicsIsland.h
+++ Source/Deadbrick/World/VoxelPhysicsIsland.h
@@ -2,9 +2,11 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "World/DeadbrickVoxelTypes.h"
 #include "VoxelPhysicsIsland.generated.h"
 
 class ADestructibleVoxelWorld;
+class UPrimitiveComponent;
 class UProceduralMeshComponent;
 
 UCLASS()
@@ -15,6 +17,16 @@ class DEADBRICK_API AVoxelPhysicsIsland : public AActor
 public:
     AVoxelPhysicsIsland();
     virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+    virtual float TakeDamage(float DamageAmount, const FDamageEvent& DamageEvent, AController* EventInstigator, AActor* DamageCauser) override;
+    virtual void NotifyHit(
+        UPrimitiveComponent* MyComp,
+        AActor* Other,
+        UPrimitiveComponent* OtherComp,
+        bool bSelfMoved,
+        FVector HitLocation,
+        FVector HitNormal,
+        FVector NormalImpulse,
+        const FHitResult& Hit) override;
 
     UFUNCTION()
     void InitializeFromVoxels(ADestructibleVoxelWorld* SourceWorld, const TArray<FIntVector>& Voxels, bool bStartSimulating = true);
@@ -26,10 +38,20 @@ private:
     UPROPERTY(VisibleAnywhere)
     TObjectPtr<UProceduralMeshComponent> MeshComponent;
 
+    UPROPERTY()
+    TWeakObjectPtr<ADestructibleVoxelWorld> SourceVoxelWorld;
+
     float PreparedMassKg = 1.0f;
     FVector PreparedLocalCenter = FVector::ZeroVector;
     FVector PreparedHalfExtents = FVector(10.0f);
     int64 PhysXBodyHandle = INDEX_NONE;
     bool bPreparedForPhysics = false;
     bool bUsingPhysX = false;
+
+    int32 SourceVoxelCount = 0;
+    float RubbleDurability = 100.0f;
+    EDeadbrickVoxelMaterial DominantMaterial = EDeadbrickVoxelMaterial::Concrete;
+
+    void ApplyGameplayImpulse(const FVector& Impulse);
+    void BreakIntoSalvage();
 };
