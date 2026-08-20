74146ce846f4e0ae3a9a7db856e1a52f18ea75e7
ramonsamagaio <70339182+ramonsamagaio@users.noreply.github.com>
2026-08-20 05:20:41 -0300

    Bump gameplay depth build marker

--- Source/Deadbrick/Deadbrick.cpp
+++ Source/Deadbrick/Deadbrick.cpp
@@ -7,7 +7,7 @@ public:
     virtual void StartupModule() override
     {
         FDefaultGameModuleImpl::StartupModule();
-        UE_LOG(LogTemp, Display, TEXT("DEADBRICK MODULE BUILD 2026-08-20-R GRANULAR-RUBBLE-ITEMS-BUILDING-DETAIL LOADED"));
+        UE_LOG(LogTemp, Display, TEXT("DEADBRICK MODULE BUILD 2026-08-20-S GRAVITY-COLLAPSE-INVENTORY-ZOMBIES-WEAPON-CITY LOADED"));
     }
 };
 
